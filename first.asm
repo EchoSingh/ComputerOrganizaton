be36ac14fed7a20aa79a2e6436a50f15d2f8abdf
ADITYA SINGH <adityarathore2606@gmail.com>
2024-05-10 12:02:59 +0530

    name program

---
+++ first.asm
@@ -0,0 +1,25 @@
+
+.MODEL SMALL  
+.STACK 100H  
+.DATA  
+  
+STRING DB ' I am Aitya Singh', '$'
+  
+.CODE  
+MAIN PROC FAR  
+ MOV AX,@DATA  
+ MOV DS,AX  
+  
+
+ LEA DX,STRING  
+  
+
+ MOV AH,09H 
+ INT 21H  
+  
+
+ MOV AH,4CH 
+ INT 21H  
+  
+MAIN ENDP  
+END MAIN 
